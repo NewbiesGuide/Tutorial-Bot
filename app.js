631825fa8fc15022fafc3735ad56bff0e7201f58
NewbiesGuide <31818428+NewbiesGuide@users.noreply.github.com>
2017-09-11 19:29:19 +0800

    Episode 2

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const Discord = require('discord.js');
 // Defines client as the Bot User
 const client = new Discord.Client()
+    // Let us define config
+const config = require('./config.json');
 
 client.on('ready', () => {
     // For when it goes Online
@@ -10,11 +12,19 @@ client.on('ready', () => {
 
 client.on('message', message => {
     // Defines message as the messages for the Bot
-    if (message.content === "ping") {
-        message.reply(`Pong`)
+    if (message.content.startsWith(config.prefix + "ping")) {
+        // So usage will be, ;;ping
+        message.reply(`Pong!`)
             // If your message is ping, reply Pong!
+    } else
+    // Another Statement
+    if (message.content.startsWith(config.prefix + "food")) {
+        // Usage is ;;food
+        message.reply('Yay! Food')
+            // Replies Yay! Food
     }
 });
 
 // Let's let the bot Login
-client.login('NO TOKEN FOR YOU!')
+client.login(config.token)
+    // Now , we make it login using the Config file!
